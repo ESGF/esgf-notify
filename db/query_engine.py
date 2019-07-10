777cecb2f1bd8dc8fd1d1446a6cff5b4f458288c
Sasha Ames <amysash2006@gmail.com>
2019-07-10 16:08:31 -0700

    change to generator

--- db/query_engine.py
+++ db/query_engine.py
@@ -12,18 +12,35 @@ class QueryEngine():
 	def __init__(self, dburl):
 		engine = create_engine(dburl, echo=False)
 		self.Session = sessionmaker(bind=engine)
-
-
-	def get_rows(self):
+		self.lastid = -1
 
 		session = self.Session()
 
-		res = session.query(ESGFSubscribers,ESGFUser.email,ESGFTerms.keyname,ESGFTerms.valuename).join(ESGFUser).join(ESGFTerms)
+		self.query_res = session.query(ESGFSubscribers,ESGFUser.email,ESGFTerms.keyname,ESGFTerms.valuename).join(ESGFUser).join(ESGFTerms)
+		session.close()
 
+	def get_rows():
 
-		for x in res:
-			print str(x)
+		result_dict = {}
+		last_id = -1
+		email = ''
+		for x in self.query_res:
+
+			if not last_id 	== x[0].id:
+				if last_id > -1
+					yield (x[1], result_dict)
+			last_id = x[0].id
+			result_dict[x[2]] = x[3]
+			email=x[1]
+
+		yield (email, result_dict)
 
 
 qe = QueryEngine('postgresql://dbsuper:esgrocks@localhost/esgcet')
-qe.get_rows()
+
+for i, x in enumerate qe.get_rows():
+
+	print i + 1, str(x)
+
+
+
