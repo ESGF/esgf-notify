5e669410f5185d0924b563c7d38bfd959c341f08
Sasha Ames <amysash2006@gmail.com>
2019-07-29 16:28:40 -0700

    initial

---
+++ test/list2json.py
@@ -0,0 +1,66 @@
+
+
+DRS = { 'CMIP6' : [ 'mip_era' , 'activity_drs','institution_id','source_id','experiment_id','member_id','table_id','variable_id','grid_label', 'version' ] }
+
+
+from random import sample
+
+def get_rand_lines(infile, count):
+
+	return sample([line for line in infile], count)
+
+
+def list_to_json(in_arr, node):
+
+	ret = []
+	for line in in_arr:
+
+		parts = line.split('.')
+
+		key = parts[0]
+		facets = DRS[key]
+		d = {}
+		for i, f in enumerate(facets):
+			d[f] = parts[i]
+
+		d['data_node'] = node
+		d['index_node'] = node
+		DRSlen = len(DRS[key])
+		instance_id = '.'.join(parts[0:DRSlen])
+		d['instance_id'] = instance_id
+		d['master_id'] = '.'.join(parts[0:DRSlen-1])
+		d['id'] = instance_id + '|' + node
+		d['title'] = instance_id
+		d['replica'] = 'false'
+		d['latest'] = 'true'
+		d['type'] = 'Dataset'
+		d['project'] = key + '-test'
+
+		d['version'] = d['version'][1:]
+		ret.append(d)
+
+	return ret
+
+
+def gen_xml(fn, d):
+	f=open(fn,'w')
+	f.write("<doc>\n")
+	for key in d:
+		f.write('  <field name="{}">{}</field>'.format(key, d[key]))
+	f.write("</doc>\n")
+	f.close()
+
+
+import sys
+
+d = list_to_json(get_rand_lines(sys.stdin, int(sys.argv[1])), 'esgf-test-data.llnl.gov')
+
+if len(sys.argv > 2):
+
+	path_prefix = sys.argv[2]
+
+for rec in d:
+
+	gen_xml(path_prefix+rec['instance_id'] + '.xml', rec)
+
+
