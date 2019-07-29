028e3792fd68224ff4aadce7181dd6d78574f377
Sasha Ames <amysash2006@gmail.com>
2019-07-29 16:34:15 -0700

    fix args

--- test/list2json.py
+++ test/list2json.py
@@ -55,7 +55,7 @@ import sys
 
 d = list_to_json(get_rand_lines(sys.stdin, int(sys.argv[1])), 'esgf-test-data.llnl.gov')
 
-if len(sys.argv > 2):
+if len(sys.argv) > 2:
 
 	path_prefix = sys.argv[2]
 
