d8f851629529f3559baedf9e4052168acf2c80d0
sashakames <ames4@llnl.gov>
2019-08-29 14:15:46 -0700

    fixindent

--- test/pub_test.py
+++ test/pub_test.py
@@ -10,27 +10,26 @@ ARGS = 1
 
 def main(args):
 
-	if len(args) < (ARGS + 1):
-		print("Missing required arguments")
-		exit(0)
+    if len(args) < (ARGS + 1):
+        print("Missing required arguments")
+        exit(0)
 
 
-	pubcount = int(args[1])
-#	hostname = args[1]
-#	cert_fn = args[3]
+    pubcount = int(args[1])
+    #	hostname = args[1]
+    #	cert_fn = args[3]
 
-	d = list2json.list_to_json(list2json.get_rand_lines(sys.stdin, pubcount), hostname, increment=increment_in)
+    d = list2json.list_to_json(list2json.get_rand_lines(sys.stdin, pubcount), hostname, increment=increment_in)
 
-	pubCli = publisherClient(cert_fn, hostname)
+    pubCli = publisherClient(cert_fn, hostname)
 
-	for rec in d:
+    for rec in d:
 
-		new_xml = list2json.gen_xml(rec)
-		if increment_in:
-			upd_xml = list2json.gen_hide_xml(rec['prev_id'])
-
-		pubCli.update(upd_xml)
-		pubCli.publish(new_xml)
+        new_xml = list2json.gen_xml(rec)
+        if increment_in:
+            upd_xml = list2json.gen_hide_xml(rec['prev_id'])
+            pubCli.update(upd_xml)
+        pubCli.publish(new_xml)
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
