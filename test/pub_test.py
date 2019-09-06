45721d65423de73f88123300d9e34a126e124216
sashakames <ames4@llnl.gov>
2019-09-06 16:32:20 -0700

    add retraction

--- test/pub_test.py
+++ test/pub_test.py
@@ -4,7 +4,8 @@ import list2json, sys
 
 hostname = "pcmdi8vm.llnl.gov"
 cert_fn = "cert.pem"
-increment_in = True
+increment_in = False
+retract = False
 
 ARGS = 1
 
@@ -19,17 +20,24 @@ def main(args):
     #	hostname = args[1]
     #	cert_fn = args[3]
 
-    d = list2json.list_to_json(list2json.get_rand_lines(sys.stdin, pubcount), hostname, increment=increment_in)
-
     pubCli = publisherClient(cert_fn, hostname)
 
-    for rec in d:
+    if retract:
 
-        new_xml = list2json.gen_xml(rec)
-        if increment_in:
-            upd_xml = list2json.gen_hide_xml(rec['prev_id'])
-            pubCli.update(upd_xml)
-        pubCli.publish(new_xml)
+        for dset in list2json.get_rand_lines(sys.stdin, pubcount):
+            dset_id = "{}|{}".format(dset, hostname)
+            pubCli.retract(dset_id)
+    else:
+        d = list2json.list_to_json(list2json.get_rand_lines(sys.stdin, pubcount), hostname, increment=increment_in)
+
+
+        for rec in d:
+
+            new_xml = list2json.gen_xml(rec)
+            if increment_in:
+                upd_xml = list2json.gen_hide_xml(rec['prev_id'])
+                pubCli.update(upd_xml)
+            pubCli.publish(new_xml)
 
 if __name__ == '__main__':
     main(sys.argv)
